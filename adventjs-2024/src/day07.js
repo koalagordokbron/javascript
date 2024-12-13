c66326b337e9f2a5ab666430d710c9bf678ff5ab
koalagordokbron <pablomorcillocuenca@gmail.com>
2024-12-13 10:57:05 +0100

    Day 07 of AdventJS 2024 by Midudev. 5 stars solutions

---
+++ adventjs-2024/src/day07.js
@@ -0,0 +1,10 @@
+/** @param {string} packages with parentheses
+ *  @returns {string} Fixed and sorted packages
+ */
+function fixPackages(packages) {
+  while(packages.indexOf('(') > -1) {
+    packages = packages.replace(/\((\w+)\)/, (_, v) => [...v].reverse().join(''))
+  }
+  
+  return packages
+}
